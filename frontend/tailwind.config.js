8dfe7b9165a410f3d29f094f1787450c0e3a371b
kennedy <sferreira.kennedy@gmail.com>
2024-07-30 16:48:12 +0100

    create Header

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto Slab", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
-
